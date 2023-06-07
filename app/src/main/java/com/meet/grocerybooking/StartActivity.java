2787208e4584e8df6b0ba967f3f3e0000b4f4c29
Meet Patel <pmeet007m@gmail.com>
2023-06-07 13:07:47 +0530

    save point

--- app/src/main/java/com/meet/grocerybooking/StartActivity.java
+++ app/src/main/java/com/meet/grocerybooking/StartActivity.java
@@ -2,7 +2,13 @@ package com.meet.grocerybooking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.meet.grocerybooking.authentication.AdminLogInActivity;
+import com.meet.grocerybooking.authentication.ShopkeeperLogInActivity;
+import com.meet.grocerybooking.authentication.UserLogInActivity;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,5 +16,21 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        Button btnUser = findViewById(R.id.btn_user_login);
+        Button btnShopkeeper = findViewById(R.id.btn_shopkeeper_login);
+        Button btnAdmin = findViewById(R.id.btn_admin_login);
+
+        btnUser.setOnClickListener(v -> {
+            startActivity(new Intent(StartActivity.this, UserLogInActivity.class));
+        });
+
+        btnShopkeeper.setOnClickListener(v -> {
+            startActivity(new Intent(StartActivity.this, ShopkeeperLogInActivity.class));
+        });
+
+        btnAdmin.setOnClickListener(v -> {
+            startActivity(new Intent(StartActivity.this, AdminLogInActivity.class));
+        });
     }
 }
